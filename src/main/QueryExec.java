454f3934ac36131c95737f69bac46521b36ee878
talipov_mi_talipov.mars <t.mars@mail.ru>
2014-04-13 11:28:02 +0300

    changed dir structure

---
+++ src/main/QueryExec.java
@@ -0,0 +1,12 @@
+import java.io.FileInputStream;
+import org.antlr.runtime.*;
+
+public class QueryExec {
+    public static void main(String[] args) throws Exception {
+        ANTLRFileStream in = new ANTLRFileStream(args[0]);
+    	SparqlLexer lexer = new SparqlLexer(in);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SparqlParser parser = new SparqlParser(tokens);
+        parser.query(); 
+    }
+}
