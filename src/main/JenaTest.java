62b35155b987da4df4882addc07ba4c42143b592
talipov_mi_talipov.mars <t.mars@mail.ru>
2014-04-19 20:14:16 +0300

    added jena api and JenaTest program

---
+++ src/main/JenaTest.java
@@ -0,0 +1,28 @@
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+public class JenaTest {
+    public static void main(String[] args) throws Exception {
+        Model m = ModelFactory.createDefaultModel();
+        String nsA = "http://somewhere/else#";
+        String nsB = "http://nowhere/else#";
+        Resource root = m.createResource( nsA + "root" );
+        Property P = m.createProperty( nsA + "P" );
+        Property Q = m.createProperty( nsB + "Q" );
+        Resource x = m.createResource( nsA + "x" );
+        Resource y = m.createResource( nsA + "y" );
+        Resource z = m.createResource( nsA + "z" );
+        m.add( root, P, x ).add( root, P, y ).add( y, Q, z );
+        System.out.println( "# -- no special prefixes defined" );
+        m.write( System.out );
+        System.out.println( "# -- nsA defined" );
+        m.setNsPrefix( "nsA", nsA );
+        m.write( System.out );
+        System.out.println( "# -- nsA and cat defined" );
+        m.setNsPrefix( "cat", nsB );
+        m.write( System.out );
+    }
+}
